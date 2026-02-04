644584aaac2705dfbd013bd5aea5c6bb3072f816
misaelgocu <misaelgomez00000@gmail.com>
2026-02-04 23:38:47 +0000

    modifivcacion de proucion

--- kfc/settings.py
+++ kfc/settings.py
@@ -157,7 +157,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
